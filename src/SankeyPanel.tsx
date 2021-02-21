7d0d0c139f680e840238c42b3ec4861fd2436109
ismael <ismael.ubk@gmail.com>
2021-02-21 09:50:05 -0500

    wip: ussing react state for chart re render

--- src/SankeyPanel.tsx
+++ src/SankeyPanel.tsx
@@ -1,55 +1,60 @@
 // @ts-nocheck
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import * as d3 from 'd3';
-import * as d3Sankey from 'd3-sankey';
 import { PanelProps } from '@grafana/data';
 import { SankeyOptions } from 'types';
 import { Sankey } from 'Sankey'
+import { packSiblings, svg } from 'd3';
 
 interface Props extends PanelProps<SankeyOptions> {}
 
 export const SankeyPanel: React.FC<Props> = ({ options, data, width, height }) => {
-  // -----------------------    CHART CONSTANTS    -----------------------
+  // ------------------------    CHART CONSTANTS    -----------------------
   const CHART_REQUIRED_FIELDS = { source: 'source', target: 'target', value: 'value' };
 
-  // -----------------------  CHART CONFIGURATION  -----------------------
-  const config = {
-    background: '#f8f8fa',
-    align: options.align,
-    colorScheme: options.colorScheme,
-    edgeColor: options.edgeColor,
-    displayValues: options.displayValues,
-    highlightOnHover: options.highlightOnHover,
-  };
+  // ------------------------    ERROR MESSAGES    ------------------------
+  const requiredFieldsMsg = `Required fields not present: ${Object.keys(CHART_REQUIRED_FIELDS).join(', ')}`;
 
-  // ----------------------- BASE DATA ACQUISITION -----------------------
-  const frame = data.series[0];
-  const dataLen = frame.length;
-  const indices = d3.range(dataLen);
+  // -------------------------    REACT HOOKS    --------------------------
+  const [ error, setError ] = useState({ isError: false, message: '', type: '' })
+  const [ graph, setGraph ] = useState({ nodes: [], links: [] })
 
-  // -----------------------       ACCESSORS      -----------------------
-  const sourceAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.source);
-  const targetAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.target);
-  const valueAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.value);
+  useEffect(() => {
+    console.log('data changed')
+    setGraph(buildGraph())
+  }, [data])
 
-  // -----------------------      VALIDATIONS     -----------------------
-  if (!(sourceAccesor && targetAccesor && valueAccesor)) {
-    throw new Error(`Required fields not present: ${Object.keys(CHART_REQUIRED_FIELDS).join(', ')}`);
+  // -------------------------  DATA ACQUISITION  -------------------------
+  const buildGraph = () => {
+    const frame = data.series[0];
+
+    const sourceAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.source);
+    const targetAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.target);
+    const valueAccesor = frame.fields.find(field => field.name === CHART_REQUIRED_FIELDS.value);
+
+    // -----------------------      VALIDATIONS     -----------------------
+    if (!(sourceAccesor && targetAccesor && valueAccesor)) {
+      setError({ isError: true, message: requiredFieldsMsg, type: '' })
+      return { nodes: [], links: []};
+    }
+    setError({})
+
+    const sources = sourceAccesor.values.toArray();
+    const targets = targetAccesor.values.toArray();
+    const values = valueAccesor.values.toArray();
+  
+    const zip = d3.zip(sources, targets, values);
+  
+    const nodes = Array.from(new Set(sources.concat(targets))).map(node => ({ name: node }));
+    const links = zip.map(d => ({ source: d[0], target: d[1], value: +d[2].toFixed(2) }));
+    const graph = { nodes, links };
+
+    return graph
   }
-
-  // -----------------------  CHART FIELD VALUES  -----------------------
-  const sources = sourceAccesor.values.toArray();
-  const targets = targetAccesor.values.toArray();
-  const values = valueAccesor.values.toArray();
-
-  const zip = d3.zip(sources, targets, values);
-
-  const nodes = Array.from(new Set(sources.concat(targets))).map(node => ({ name: node }));
-  const links = zip.map(d => ({ source: d[0], target: d[1], value: +d[2].toFixed(2) }));
-  const graph = { nodes, links };
 
   // ------------------------------- CHART  ------------------------------
   const chart = svg => {
+    console.log('enterchart')
     const sankey = new Sankey(svg)
       .width(width)
       .height(height)
@@ -63,7 +68,11 @@ export const SankeyPanel: React.FC<Props> = ({ options, data, width, height }) =
     sankey.render()
   };
 
-  return (
+  return (error.isError ?
+    <p>
+      {error.message}
+    </p>
+    :
     <svg
       viewBox={`0 0 ${width} ${height}`}
       ref={node => {
